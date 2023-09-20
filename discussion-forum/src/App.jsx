65274dc849de312034215352c6255eb4c8634acc
Sannamari Pynnönen <sannamari.pynnonen@solita.fi>
2023-09-20 13:28:56 +0300

    Error handling for adding a new topic

--- discussion-forum/src/App.jsx
+++ discussion-forum/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import Alert from '@mui/material/Alert'
 
 import TopicTable from './components/TopicTable'
 
@@ -11,6 +12,9 @@ import topicService from './services/topics'
 function App() {
   const [topics, setTopics] = useState([])
   const [topic, setTopic] = useState('')
+  const [alert, setAlert] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [alertSeverity, setAlertSeverity] = useState('')
 
   useEffect(() => {
     topicService
@@ -22,20 +26,45 @@ function App() {
 
   const addTopic = (event) => {
     event.preventDefault()
-    const topicObject = {
-      topic_name: topic
-    }
-    topicService
-      .add(topicObject)
-      .then(returnedTopic => {
-        setTopics(topics.concat(returnedTopic))
+    
+    if (topic != ''){
+      const result = topics.find(({name}) => name === topic)
+      if (result && result.name === topic) {
+        setAlert(true)
+        setAlertMessage('Topic is already added')
+        setAlertSeverity('error')
         setTopic('')
-      })
+      } else {
+        const topicObject = {
+          name: topic
+        }
+        topicService
+          .add(topicObject)
+          .then(returnedTopic => {
+            setTopics(topics.concat(returnedTopic))
+            setTopic('')
+            setAlert(true)
+            setAlertMessage(`Added a new topic "${topic}"`)
+            setAlertSeverity('success')
+          }) 
+        } 
+    } else {
+        setAlert(true)
+        setAlertMessage('Add a topic')
+        setAlertSeverity('error')
+        
+      }
+      setTimeout(() => {
+        setAlertMessage('')
+        setAlertSeverity('')
+        setAlert(false)
+      }, 3000)
   }
 
  return (
    <div>
       <h1>Discussion forum</h1>
+      {alert ? <Alert severity={alertSeverity}>{alertMessage}</Alert> : <></> }
       <TextField 
         id="new-topic" 
         variant="outlined" 
