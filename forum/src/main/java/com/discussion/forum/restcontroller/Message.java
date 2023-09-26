9c5069b392e876d7d7e00043fc1e3d04d3fe39c4
Sannamari Pynnönen <sannamari.pynnonen@solita.fi>
2023-09-26 12:13:34 +0300

    Create Message entity, controller and repository

---
+++ forum/src/main/java/com/discussion/forum/restcontroller/Message.java
@@ -0,0 +1,84 @@
+package com.discussion.forum.restcontroller;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="messages")
+public class Message implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private int id;
+   
+    @Column(name="message")
+    private String message;
+
+    @CreationTimestamp
+    @Column(name="timestamp")
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Topic topic;
+
+    protected Message() {}
+
+    public Message(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setmessage(String message) {
+        this.message = message;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Topic geTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+}
