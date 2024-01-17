9d469f280787dbe127cd8040b8fafa34df4ba5f6
Sannamari Pynnönen <sannamari.pynnonen@solita.fi>
2024-01-17 09:30:45 +0200

    Mask banned words from messages

--- forum/src/main/java/com/discussion/forum/restcontroller/MessageController.java
+++ forum/src/main/java/com/discussion/forum/restcontroller/MessageController.java
@@ -1,7 +1,10 @@
 package com.discussion.forum.restcontroller;
 
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,23 +27,98 @@ public class MessageController {
     @Autowired
     UserRepository userRepository;
 
+    String[] bannedWords = {
+                            "ass",
+                            "asshole",
+                            "bastard",
+                            "bitch",
+                            "bloody",
+                            "bollocks",
+                            "bugger",
+                            "bullshit",
+                            "cock",
+                            "crap",
+                            "cunt",
+                            "damn",
+                            "dick",
+                            "fuck",
+                            "hell",
+                            "nigga",
+                            "piss",
+                            "prick",
+                            "pussy",
+                            "shit",
+                            "slut",
+                            "spastic",
+                            "turd",
+                            "twat",
+                            "wanker"
+    };
+    String replacement = "*";
+
     @GetMapping("/topics/{id}/messages")
     List<MessageStatistics> findByTopicId(@PathVariable int id) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
+
         
         if (principal instanceof UserDetails) {
             username = ((UserDetails)principal).getUsername();
         } else {
             username = principal.toString();
         } 
-        return messageRepository.getMessagesByTopicId(id, userRepository.findByUsername(username).getId());
+        List<MessageStatistics> messages = messageRepository.getMessagesByTopicId(id, userRepository.findByUsername(username).getId());
+        for (MessageStatistics message : messages) {
+            String maskedMessage = message.getMessage().getMessage();
+            
+            for (String bannedWord : bannedWords) {
+                maskedMessage = maskedMessage.replaceAll("(?i)\\b" + bannedWord + "\\b", replacement);
+            }
+            
+            message.getMessage().setMessage(maskedMessage);
+
+            /* 
+            String.join(" ", Arrays.asList(message.getMessage().getMessage().split(" ")).stream().map(s -> {
+                                                            if (Arrays.asList(bannedWords).contains(s)) {
+                                                                return replacement;
+                                                            } else {
+                                                                return s;
+                                                            }
+                                                        }).toList()); 
+            */
+
+            /* 
+            for (String bannedWord : bannedWords) {
+                ArrayList<String> maskedMessage = new ArrayList<String>();
+                for (String word : message.getMessage().getMessage().split("\\s+")) {
+                    System.out.println(word);
+                    // System.out.println(message.getMessage().getMessage());
+                    maskedMessage.add(StringUtils.replaceIgnoreCase(
+                                                            word, 
+                                                            bannedWord, 
+                                                            replacement
+                                                        ));
+                }
+                message.getMessage().setMessage(String.join(" ", maskedMessage));
+            } 
+            */
+        }
+        return messages;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/topics/{id}/messages")
     Message createNew(@RequestBody Message newMessage) {
-        return messageRepository.save(newMessage);
+        messageRepository.save(newMessage);
+        String maskedMessage = newMessage.getMessage();
+            
+            for (String bannedWord : bannedWords) {
+                maskedMessage = maskedMessage.replaceAll("(?i)\\b" + bannedWord + "\\b", replacement);
+            }
+            
+        newMessage.setMessage(maskedMessage);
+
+        return newMessage;
     }
 
     @PutMapping("topics/{id}/messages/{msg_id}")
